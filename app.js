5e8d3a573231b17e78832b7cec4762b20943c9af
vyralle <achia@hubspot.com>
2022-11-28 19:15:54 +0800

    added body parser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const https = require('https');
 const cookieParser = require('cookie-parser');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
@@ -24,6 +24,7 @@ MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) =
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
+app.use(bodyParser.urlencoded ({extended:false}));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
