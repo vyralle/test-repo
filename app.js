09f96096e0da3ae9b6049ed1659d3ab9325f0986
vyralle <achia@hubspot.com>
2022-11-28 18:51:15 +0800

    fixed syntax errors

--- app.js
+++ app.js
@@ -32,50 +32,59 @@ app.post("/", function(req, res) {
         console.log('saved to database');
         
         function formv3(){
-            
-            var xhr = new XMLHttpRequest();
-            var url = 'https://api.hsforms.com/submissions/v3/integration/submit/9059838/77c8fef7-52ad-45b0-9e13-c0b50357c441’
-
-            var data = {
-                "fields": [
+            function formv3(){
+                // Create the new request 
+                var xhr = new XMLHttpRequest();
+                var url = 'https://api.hsforms.com/submissions/v3/integration/submit/9059838/77c8fef7-52ad-45b0-9e13-c0b50357c441'
+                
+                // Example request JSON:
+                var data = {
+                  "fields": [
                     {
-                        "name": "email",
-                        "value": req.body.email
+                      "name": "email",
+                      "value": req.body.email
                     },
                     {
-                        "name": "firstname",
-                        "value": req.body.firstname
+                      "name": "firstname",
+                      "value": req.body.firstname
                     }
-                ],
-                "context": {
+                  ],
+                  "context": {
                     "hutk": req.cookies.hubspotutk,
-                    "pageUri": "https://thiswassubmittedonlocalhost.com",
-                    "pageName": "Local host submission"
+                    "pageUri": "http://www.portfolio.com/contact",
+                    "pageName": "Portfolio contact me"
+                  }
                 }
-            }
-
-            var final_data = JSON.stringify(data);
+            
+                var final_data = JSON.stringify(data)
+            
+            
             xhr.open('POST', url);
-
+            // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
             xhr.setRequestHeader('Content-Type', 'application/json');
-
+        
             xhr.onreadystatechange = function() {
-                 if(xhr.readyState == 4 && xhr.status == 200) { 
+                if(xhr.readyState == 4 && xhr.status == 200) { 
                     console.log(xhr.responseText); // Returns a 200 response if the submission is successful.
-                 } else if (xhr.readyState == 4 && xhr.status == 400){ 
+                } else if (xhr.readyState == 4 && xhr.status == 400){ 
                     console.log(xhr.responseText); // Returns a 400 error the submission is rejected.          
-                 } else if (xhr.readyState == 4 && xhr.status == 403){ 
+                } else if (xhr.readyState == 4 && xhr.status == 403){ 
                     console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
-                 } else if (xhr.readyState == 4 && xhr.status == 404){ 
+                } else if (xhr.readyState == 4 && xhr.status == 404){ 
                     console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+                }
+               }
+        
+        
+            // Sends the request 
+            
+            xhr.send(final_data)
+         }
+        
+         formv3();
         }
-       }
-    // Sends the request 
-    xhr.send(final_data)
- }
-    formv3();
-        }
-    });
+    })
+});
 
 
     app.listen(3000, () => {
